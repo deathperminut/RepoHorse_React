b51cad01844b92ea14ecfa8bc7f9d94c1fa17078
Sebastian Mendez <mendezsebas8@gmail.com>
2022-12-14 16:27:46 -0500

    Se finaliza pestaña inicio

--- src/Landing/Inicio/Inicio.jsx
+++ src/Landing/Inicio/Inicio.jsx
@@ -15,7 +15,10 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 /* IMAGE */
-import Backgroundcolumn from '../../Sources/Images/Landing/Landing1.png'
+import Backgroundcolumn from '../../Sources/Images/Landing/Landing1.png';
+import BackgroundImage from '../../Sources/Images/Landing/fondo.png';
+import HorseAppBackGround from '../../Sources/Images/Landing/HorseAppBack.png';
+import Logo from '../../Sources/Images/Landing/Logo.png'
 
 export default function Inicio() {
   /* NAVIGATE  */
@@ -30,9 +33,9 @@ export default function Inicio() {
   
   return (
     <React.Fragment>
-      <div className='InitContainer'>
+      <div className='InitContainer' style={{ backgroundImage: `url(${BackgroundImage})` ,backgroundSize:'cover'}}>
         <div className='HeaderContainer'>
-              <div class="left-inner-addon input-container">
+              <div class="input-container">
                   <Icon.Search className='IconInput'/>
                   <input type="text"
                         class="InputText" 
@@ -43,18 +46,32 @@ export default function Inicio() {
         <div className='ServicesContainer'>
             <div className='ContainerRows'>
               <div className="row">
-                <div  className="col HorseApp" xs>
-                     <FaHorseHead className='col-Icon'/>
-                     <h4 className="HorseAppTitle col-element">Mr horse</h4>
+                <div  className="col HorseApp" xs style={{ backgroundImage: `url(${HorseAppBackGround})` ,backgroundSize:'cover' }}>
+                     <img  className='Logo' src={Logo} alt=""></img>
                      <Button variant="outline" onClick={RedirectLogin} className="button_18">Ingresar</Button>
                 </div>
-                <div className="col" style={{backgroundImage:Backgroundcolumn}}>Desarrollos futuros</div>
-                <div className="col" style={{backgroundImage:Backgroundcolumn}}>Desarrollos futuros</div>
+                <div className="col position-relative">
+                    <img className='overlayCard position-absolute top-50 start-50 translate-middle' src={Backgroundcolumn} alt=""></img>
+                    <p className="m-0 p-0 position-absolute top-50 start-50 translate-middle">Desarrollos futuros</p>
+                </div>
+                <div className="col">
+                    <img className='overlayCard position-absolute top-50 start-50 translate-middle' src={Backgroundcolumn} alt=""></img>
+                    <p className="m-0 p-0 position-absolute top-50 start-50 translate-middle">Desarrollos futuros</p>
+                </div>
               </div>
               <div className="row">
-                <div className="col" style={{backgroundImage:Backgroundcolumn}}>Desarrollos futuros</div>
-                <div className="col" style={{backgroundImage:Backgroundcolumn}} >Desarrollos futuros</div>
-                <div className="col" style={{backgroundImage:Backgroundcolumn}}>Desarrollos futuros</div>
+                <div className="col">
+                    <img className='overlayCard position-absolute top-50 start-50 translate-middle' src={Backgroundcolumn} alt=""></img>
+                    <p className="m-0 p-0 position-absolute top-50 start-50 translate-middle">Desarrollos futuros</p>
+                </div>
+                <div className="col">
+                    <img className='overlayCard position-absolute top-50 start-50 translate-middle' src={Backgroundcolumn} alt=""></img>
+                    <p className="m-0 p-0 position-absolute top-50 start-50 translate-middle">Desarrollos futuros</p>
+                </div>
+                <div className="col">
+                    <img className='overlayCard position-absolute top-50 start-50 translate-middle' src={Backgroundcolumn} alt=""></img>
+                    <p className="m-0 p-0 position-absolute top-50 start-50 translate-middle">Desarrollos futuros</p>
+                </div>
               </div>
            </div>
         </div>
