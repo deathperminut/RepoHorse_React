cdf69d72ebd62afbf5dab78d2ce6ad374fb2b4d9
Sebastian Mendez <mendezsebas8@gmail.com>
2023-02-09 22:47:37 -0500

    "Cambios modal"

--- src/Shared/Modal_image/modal_image.js
+++ src/Shared/Modal_image/modal_image.js
@@ -1,9 +1,8 @@
 import './modal_image.css';
 import Modal from 'react-bootstrap/Modal';
 import Carousel from 'react-bootstrap/Carousel';
-import image from '../../Sources/Images/Estadisticas/imagen_4.png';
-import image2 from '../../Sources/Images/Estadisticas/imagen_2_carusell.png';
-import image3 from '../../Sources/Images/Estadisticas/imagen_3.png';
+import image from '../../Sources/Images/Estadisticas/Imagen_modal_1.png';
+import image2 from '../../Sources/Images/Estadisticas/Imagen_modal_3.png';
 
 
 import React from 'react'
@@ -20,13 +19,11 @@ export default function Modal_image(props) {
 
         >
             <Modal.Header closeButton className='modal_header'>
-            <Modal.Title id="contained-modal-title-vcenter bold-size">
-                <span className="bold-size ml-30px">Posición óptima modelo</span>
-            </Modal.Title>
             </Modal.Header>
             <Modal.Body className='modal_content_2'>
             <Carousel className='align-center'>
             <Carousel.Item>
+                <p className='textImage_1'>Asegúrate que tu caballo esté en la posición correcta y sin obstrucciones</p>
                 <img
                 className="imageClass"
                 src={image}
@@ -35,19 +32,13 @@ export default function Modal_image(props) {
 
             </Carousel.Item>
             <Carousel.Item>
+                <p className='textImage_2'>Trata de escoger un trozo de video donde la posición correcta se mantenga entre unos 5 y 7 segundos. Usa las herramientas de cortado</p>
                 <img
-                className="imageClass"
+                className="imageClass_2"
                 src={image2}
                 alt="Second slide"
                 />
 
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="imageClass"
-                src={image3}
-                alt="Third slide"
-                />
             </Carousel.Item>
             </Carousel>
 
